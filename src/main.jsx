a6a338eb9597aa42497e1b4bc840c7053f78021c
rohithaider <rohithaider@gmail.com>
2024-11-23 12:43:58 +0600

    initialization

---
+++ src/main.jsx
@@ -0,0 +1,35 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import MainLayout from './layout/MainLayout';
+import { Login } from './routes/Login';
+import { Register } from './routes/Register';
+
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <MainLayout/>,
+    children:[
+      {
+        path:"login",
+        element: <Login/>,
+      },
+      {
+        path:"register",
+        element: <Register/>,
+      }
+    ]
+  },
+]);
+
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+      <RouterProvider router={router} />
+  </StrictMode>,
+)
